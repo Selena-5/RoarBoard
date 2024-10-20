9f00b72159c3e7450b4f240f0a9cd762c60f0697
jhalsey <julianhalsey5@gmail.com>
2024-10-20 14:12:36 -0400

    Frontend of Clubs page

--- src/Clubs.js
+++ src/Clubs.js
@@ -1,8 +1,37 @@
-// Clubs.js
 import React from 'react';
 
 function Clubs() {
-    return <h1>Clubs Page</h1>;
-  }
+  return (
+    <>
+      <h3>Toggle which clubs you would like to subscribe to:</h3>
+      <h3>     </h3>
+      <div className="form-check form-switch">
+        <input className="form-check-input" type="checkbox" id="SWEClub" />
+        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Software Engineering Club</label>
+      </div>
+      <div className="form-check form-switch">
+        <input className="form-check-input" type="checkbox" id="BBCLub" />
+        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Basketball Club</label>
+      </div>
+      <div className="form-check form-switch">
+        <input className="form-check-input" type="checkbox" id="HealthClub" />
+        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Health Club</label>
+      </div>
+      <div className="form-check form-switch">
+        <input className="form-check-input" type="checkbox" id="SoccerClub" />
+        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Soccer Club</label>
+      </div>
+      <div className="form-check form-switch">
+        <input className="form-check-input" type="checkbox" id="WritingCLub" />
+        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Writing Club</label>
+      </div>
+      <div className="form-check form-switch">
+        <input className="form-check-input" type="checkbox" id="FBClub" />
+        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Football Club</label>
+      </div>
+
+    </>
+  );
+}
 
 export default Clubs;
